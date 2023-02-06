2a7be0d56deda0f49b6c37e68c7f2b1a8efb7dc4
conglt <thanhcong1710@gmail.com>
2023-02-06 23:44:02 +0700

    update

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -34,10 +34,10 @@
 </script>
 <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
-
 <script>
-    var socket = io('https://socket.cmsedu.vn', {transports: ['websocket', 'polling', 'flashsocket']});
-    socket.on('message', function(msg) {
-        console.log(msg);
-    });
+  var socket = io('https://socket.cmsedu.vn', {transports: ['websocket', 'polling', 'flashsocket']});
+  
+  socket.on('call_end', function(msg) {
+    console.log(msg);
+  });
 </script>
