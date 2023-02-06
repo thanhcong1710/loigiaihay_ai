ead7fa03d37bcc5a6abc16b64c12096cb011cc55
conglt <thanhcong1710@gmail.com>
2023-02-06 12:02:14 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        '\App\Console\Commands\JobsProcessCrawlCoPhieu68'
     ];
     /**
      * Define the application's command schedule.
