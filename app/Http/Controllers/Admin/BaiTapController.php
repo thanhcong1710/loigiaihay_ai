58aa61175e56fbb218325f2558ce636b8c62e4fb
conglt <thanhcong1710@gmail.com>
2023-02-17 14:45:10 +0700

    update

---
+++ app/Http/Controllers/Admin/BaiTapController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\UtilityServiceProvider as u;
+
+class BaiTapController extends Controller
+{
+    
+}
